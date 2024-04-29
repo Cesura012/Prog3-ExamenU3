ea978ab2971270742c70b0723824bcc80c35d007
Ernesto Gracia Nava <113970580+Cesura012@users.noreply.github.com>
2024-04-28 23:52:29 -0700

    darbaja_hecho

--- src/examen.java
+++ src/examen.java
@@ -18,6 +18,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
+import javax.swing.SwingConstants;
 
 public class examen {
 
@@ -31,6 +32,11 @@ public class examen {
 	private JTextField textField_2_c;
 	private JTextField textField_2;
 	private JTextField textField_3;
+	private JTextField textField_4_nombre;
+	private JTextField textField_4_ap;
+	private JTextField textField_4;
+	private JTextField textField_5_c;
+	private JTextField textField_5;
 
 	/**
 	 * Launch the application.
@@ -64,6 +70,238 @@ public class examen {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		JPanel panel_Baja = new JPanel();
+		panel_Baja.setBackground(new Color(192, 192, 192));
+		panel_Baja.setBounds(0, 0, 1130, 554);
+		frame.getContentPane().add(panel_Baja);
+		panel_Baja.setLayout(null);
+		
+		JLabel lblNewLabel_10 = new JLabel("Dar Baja");
+		lblNewLabel_10.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_10.setVerticalAlignment(SwingConstants.TOP);
+		lblNewLabel_10.setBounds(573, 51, 70, 20);
+		panel_Baja.add(lblNewLabel_10);
+		
+		JLabel lblNewLabel_11 = new JLabel("Nombre:");
+		lblNewLabel_11.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_11.setBounds(122, 177, 70, 33);
+		panel_Baja.add(lblNewLabel_11);
+		
+		textField_4_nombre = new JTextField();
+		textField_4_nombre.setBounds(188, 177, 153, 33);
+		panel_Baja.add(textField_4_nombre);
+		textField_4_nombre.setColumns(10);
+		
+		JLabel lblNewLabel_12 = new JLabel("Apellido Paterno:");
+		lblNewLabel_12.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_12.setBounds(122, 261, 128, 33);
+		panel_Baja.add(lblNewLabel_12);
+		
+		textField_4_ap = new JTextField();
+		textField_4_ap.setBounds(255, 261, 141, 33);
+		panel_Baja.add(textField_4_ap);
+		textField_4_ap.setColumns(10);
+		
+		JLabel lblNewLabel_13 = new JLabel("Apellido Materno:");
+		lblNewLabel_13.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_13.setBounds(122, 372, 128, 33);
+		panel_Baja.add(lblNewLabel_13);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(255, 367, 141, 33);
+		panel_Baja.add(textField_4);
+		textField_4.setColumns(10);
+		
+		JLabel lblNewLabel_14 = new JLabel("Correo Electronico:");
+		lblNewLabel_14.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_14.setBounds(597, 188, 150, 33);
+		panel_Baja.add(lblNewLabel_14);
+		
+		textField_5_c = new JTextField();
+		textField_5_c.setBounds(738, 185, 141, 33);
+		panel_Baja.add(textField_5_c);
+		textField_5_c.setColumns(10);
+		
+		JLabel lblNewLabel_15 = new JLabel("Numero de Control:");
+		lblNewLabel_15.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_15.setBounds(597, 272, 150, 33);
+		panel_Baja.add(lblNewLabel_15);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(743, 269, 141, 33);
+		panel_Baja.add(textField_5);
+		textField_5.setColumns(10);
+		
+		JButton btnNewButton_10 = new JButton("REGRESAR");
+		btnNewButton_10.setBounds(188, 480, 100, 33);
+		panel_Baja.add(btnNewButton_10);
+		btnNewButton_10.addActionListener(new ActionListener() {
+			 public void actionPerformed(ActionEvent e) {
+			      
+			        frame.getContentPane().remove(panel_Baja);
+			        frame.getContentPane().repaint();
+			        frame.getContentPane().revalidate();
+			   
+			        menuAlumno(frame);
+			    }
+		});
+		
+		JButton btnNewButton_11 = new JButton("Dar Baja");
+		btnNewButton_11.setBounds(951, 485, 89, 33);
+		panel_Baja.add(btnNewButton_11);
+		btnNewButton_11.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+		    
+		        if (textField_4_nombre.getText().isEmpty() ||
+		            textField_4_ap.getText().isEmpty() ||
+		            textField_4.getText().isEmpty() ||
+		            textField_5_c.getText().isEmpty() ||
+		            textField_5.getText().isEmpty()) {
+		
+		            textField_4_nombre.setBorder(BorderFactory.createLineBorder(Color.RED));
+		            textField_4_ap.setBorder(BorderFactory.createLineBorder(Color.RED));
+		            textField_4.setBorder(BorderFactory.createLineBorder(Color.RED));
+		            textField_5_c.setBorder(BorderFactory.createLineBorder(Color.RED));
+		            textField_5.setBorder(BorderFactory.createLineBorder(Color.RED));
+		    
+		            JOptionPane.showMessageDialog(frame, "Por favor complete todos los campos.", "Error", JOptionPane.ERROR_MESSAGE);
+		        } else {
+		      
+		            textField_4_nombre.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+		            textField_4_ap.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+		            textField_4.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+		            textField_5_c.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+		            textField_5.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+		       
+		            JOptionPane.showMessageDialog(frame, "La persona ha sido dada de baja con éxito.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+		        }
+		    }
+		});
+		
+		
+		
+	
+		
+	
+	}
+	
+	public void login(JFrame frame) {
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(0, 128, 192));
+		panel.setBounds(0, 0, 1134, 554);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("INICIO DE SESION");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblNewLabel.setBounds(442, 26, 253, 67);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Numero de Control");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_1.setBounds(432, 131, 164, 52);
+		panel.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(405, 194, 209, 42);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("CONTRASEÑA");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_1_1.setBounds(442, 264, 164, 52);
+		panel.add(lblNewLabel_1_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(405, 327, 209, 45);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("INGRESAR");
+		btnNewButton.setBackground(new Color(192, 192, 192));
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnNewButton.setBounds(698, 406, 190, 67);
+		panel.add(btnNewButton);
+		
+		JButton btnRegistrarse = new JButton("REGISTRARSE");
+		btnRegistrarse.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnRegistrarse.setBackground(Color.LIGHT_GRAY);
+		btnRegistrarse.setBounds(147, 406, 190, 67);
+		panel.add(btnRegistrarse);
+		btnRegistrarse.addActionListener(new ActionListener() {
+			 public void actionPerformed(ActionEvent e) {
+			      
+			        frame.getContentPane().remove(panel);
+			        frame.getContentPane().repaint();
+			        frame.getContentPane().revalidate();
+			   
+			        registro(frame);
+			    }
+		});
+	}
+	
+	public void menu(JFrame frame) {
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(128, 255, 255));
+		panel_1.setBounds(0, 0, 1134, 554);
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JButton btnNewButton_1 = new JButton("Alumnos");
+		btnNewButton_1.setBackground(new Color(192, 192, 192));
+		btnNewButton_1.setBounds(464, 70, 251, 109);
+		panel_1.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Docentes");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2.setBackground(new Color(192, 192, 192));
+		btnNewButton_2.setBounds(464, 263, 251, 109);
+		panel_1.add(btnNewButton_2);
+		
+	}
+	
+	public void menuAlumno(JFrame frame) {
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(128, 255, 255));
+		panel.setBounds(0, 0, 1130, 554);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JButton btnNewButton_3 = new JButton("Descargar Informacion");
+		btnNewButton_3.setBackground(new Color(192, 192, 192));
+		btnNewButton_3.setBounds(205, 114, 170, 90);
+		panel.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("Consultar");
+		btnNewButton_4.setBackground(new Color(192, 192, 192));
+		btnNewButton_4.setBounds(469, 114, 162, 90);
+		panel.add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("Crear");
+		btnNewButton_5.setBackground(new Color(192, 192, 192));
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_5.setBounds(793, 114, 162, 90);
+		panel.add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("Editar");
+		btnNewButton_6.setBackground(new Color(192, 192, 192));
+		btnNewButton_6.setBounds(213, 313, 162, 90);
+		panel.add(btnNewButton_6);
+		
+		JButton btnNewButton_7 = new JButton("Eliminar");
+		btnNewButton_7.setBackground(new Color(192, 192, 192));
+		btnNewButton_7.setBounds(469, 313, 162, 90);
+		panel.add(btnNewButton_7);
+		
+	}
+	
+	public void registro(JFrame frame) {
 		JPanel panel_RA = new JPanel();
 		panel_RA.setBackground(new Color(192, 192, 192));
 		panel_RA.setBounds(10, 5, 1120, 532);
@@ -155,7 +393,7 @@ public class examen {
 		panel_RA.add(comboBox_mes);
 		
 		String año[] = { "1989" ,"1990" ,"1991" , "1992" , "1993" , "1994" ,"1995" , "1996" , "1997" , "1998" ,"1999" , "2000" , "2001", "2002", "2003", "2004", "2005", "2006", "2007"};
-		JComboBox comboBox_año = new JComboBox(año);
+		JComboBox<Object> comboBox_año = new JComboBox<Object>(año);
 		comboBox_año.setBounds(715, 297, 60, 22);
 		panel_RA.add(comboBox_año);
 		
@@ -204,28 +442,28 @@ public class examen {
 		panel_RA.add(btnNewButton_8);
 		btnNewButton_8.addActionListener(new ActionListener() {
 		    public void actionPerformed(ActionEvent e) {
-		        // Verificar si todos los campos están llenos
+		     
 		        if (textField_2_nomres.getText().isEmpty() ||
 		            textField_2_ap.getText().isEmpty() ||
 		            textField_3_am.getText().isEmpty() ||
 		            textField_2_c.getText().isEmpty() ||
 		            textField_2.getText().isEmpty()) {
-		            // Al menos un campo está vacío, cambiar el borde a rojo
+		        
 		            textField_2_nomres.setBorder(BorderFactory.createLineBorder(Color.RED));
 		            textField_2_ap.setBorder(BorderFactory.createLineBorder(Color.RED));
 		            textField_3_am.setBorder(BorderFactory.createLineBorder(Color.RED));
 		            textField_2_c.setBorder(BorderFactory.createLineBorder(Color.RED));
 		            textField_2.setBorder(BorderFactory.createLineBorder(Color.RED));
-		            // Mostrar un mensaje de error
+		         
 		            JOptionPane.showMessageDialog(frame, "Por favor complete todos los campos.", "Error", JOptionPane.ERROR_MESSAGE);
 		        } else {
-		            // Todos los campos están llenos, cambiar el borde a verde
+		       
 		            textField_2_nomres.setBorder(BorderFactory.createLineBorder(Color.GREEN));
 		            textField_2_ap.setBorder(BorderFactory.createLineBorder(Color.GREEN));
 		            textField_3_am.setBorder(BorderFactory.createLineBorder(Color.GREEN));
 		            textField_2_c.setBorder(BorderFactory.createLineBorder(Color.GREEN));
 		            textField_2.setBorder(BorderFactory.createLineBorder(Color.GREEN));
-		            // Mostrar un mensaje de éxito
+		            
 		            JOptionPane.showMessageDialog(frame, "La persona se ha registrado con éxito.", "Éxito", JOptionPane.INFORMATION_MESSAGE);
 		        }
 		    }
@@ -236,132 +474,13 @@ public class examen {
 		panel_RA.add(btnNewButton_9);
 		btnNewButton_9.addActionListener(new ActionListener() {
 			 public void actionPerformed(ActionEvent e) {
-			        // Remueve el panel actual
+			      
 			        frame.getContentPane().remove(panel_RA);
 			        frame.getContentPane().repaint();
 			        frame.getContentPane().revalidate();
-			        // Llama al método menuAlumno
+			   
 			        menuAlumno(frame);
 			    }
 		});
-		
-	
-		
-	
-	}
-	
-	public void login(JFrame frame) {
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(0, 128, 192));
-		panel.setBounds(0, 0, 1134, 554);
-		frame.getContentPane().add(panel);
-		panel.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("INICIO DE SESION");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 17));
-		lblNewLabel.setBounds(442, 26, 253, 67);
-		panel.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Numero de Control");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblNewLabel_1.setBounds(432, 131, 164, 52);
-		panel.add(lblNewLabel_1);
-		
-		textField = new JTextField();
-		textField.setBounds(405, 194, 209, 42);
-		panel.add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("CONTRASEÑA");
-		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblNewLabel_1_1.setBounds(442, 264, 164, 52);
-		panel.add(lblNewLabel_1_1);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(405, 327, 209, 45);
-		panel.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JButton btnNewButton = new JButton("INGRESAR");
-		btnNewButton.setBackground(new Color(192, 192, 192));
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 17));
-		btnNewButton.setBounds(698, 406, 190, 67);
-		panel.add(btnNewButton);
-		
-		JButton btnRegistrarse = new JButton("REGISTRARSE");
-		btnRegistrarse.setFont(new Font("Tahoma", Font.BOLD, 17));
-		btnRegistrarse.setBackground(Color.LIGHT_GRAY);
-		btnRegistrarse.setBounds(147, 406, 190, 67);
-		panel.add(btnRegistrarse);
-		btnRegistrarse.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.getContentPane().remove(panel);
-				frame.getContentPane().repaint();;
-				frame.getContentPane().revalidate();;
-				
-				login(frame);
-			}
-		});
-	}
-	
-	public void menu(JFrame frame) {
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(128, 255, 255));
-		panel_1.setBounds(0, 0, 1134, 554);
-		frame.getContentPane().add(panel_1);
-		panel_1.setLayout(null);
-		
-		JButton btnNewButton_1 = new JButton("Alumnos");
-		btnNewButton_1.setBackground(new Color(192, 192, 192));
-		btnNewButton_1.setBounds(464, 70, 251, 109);
-		panel_1.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("Docentes");
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton_2.setBackground(new Color(192, 192, 192));
-		btnNewButton_2.setBounds(464, 263, 251, 109);
-		panel_1.add(btnNewButton_2);
-		
-	}
-	
-	public void menuAlumno(JFrame frame) {
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(128, 255, 255));
-		panel.setBounds(0, 0, 1130, 554);
-		frame.getContentPane().add(panel);
-		panel.setLayout(null);
-		
-		JButton btnNewButton_3 = new JButton("Descargar Informacion");
-		btnNewButton_3.setBackground(new Color(192, 192, 192));
-		btnNewButton_3.setBounds(205, 114, 170, 90);
-		panel.add(btnNewButton_3);
-		
-		JButton btnNewButton_4 = new JButton("Consultar");
-		btnNewButton_4.setBackground(new Color(192, 192, 192));
-		btnNewButton_4.setBounds(469, 114, 162, 90);
-		panel.add(btnNewButton_4);
-		
-		JButton btnNewButton_5 = new JButton("Crear");
-		btnNewButton_5.setBackground(new Color(192, 192, 192));
-		btnNewButton_5.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton_5.setBounds(793, 114, 162, 90);
-		panel.add(btnNewButton_5);
-		
-		JButton btnNewButton_6 = new JButton("Editar");
-		btnNewButton_6.setBackground(new Color(192, 192, 192));
-		btnNewButton_6.setBounds(213, 313, 162, 90);
-		panel.add(btnNewButton_6);
-		
-		JButton btnNewButton_7 = new JButton("Eliminar");
-		btnNewButton_7.setBackground(new Color(192, 192, 192));
-		btnNewButton_7.setBounds(469, 313, 162, 90);
-		panel.add(btnNewButton_7);
-		
 	}
 }
