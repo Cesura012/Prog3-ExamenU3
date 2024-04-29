b60870694ec8549082e5054da571fbcaa5dee8d5
Ernesto Gracia Nava <113970580+Cesura012@users.noreply.github.com>
2024-04-28 17:45:18 -0700

    menu menualumno sin acabar

--- src/examen.java
+++ src/examen.java
@@ -7,6 +7,12 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JMenuBar;
+import java.awt.GridBagLayout;
+import java.awt.CardLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class examen {
 
@@ -47,6 +53,46 @@ public class examen {
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
+		panel.setBackground(new Color(128, 255, 255));
+		panel.setBounds(0, 0, 1130, 554);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JButton btnNewButton_3 = new JButton("Descargar Informacion");
+		btnNewButton_3.setBackground(new Color(192, 192, 192));
+		btnNewButton_3.setBounds(205, 114, 170, 90);
+		panel.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("Consultar");
+		btnNewButton_4.setBackground(new Color(192, 192, 192));
+		btnNewButton_4.setBounds(469, 114, 162, 90);
+		panel.add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("Crear");
+		btnNewButton_5.setBackground(new Color(192, 192, 192));
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_5.setBounds(793, 114, 162, 90);
+		panel.add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("Editar");
+		btnNewButton_6.setBackground(new Color(192, 192, 192));
+		btnNewButton_6.setBounds(213, 313, 162, 90);
+		panel.add(btnNewButton_6);
+		
+		JButton btnNewButton_7 = new JButton("Eliminar");
+		btnNewButton_7.setBackground(new Color(192, 192, 192));
+		btnNewButton_7.setBounds(469, 313, 162, 90);
+		panel.add(btnNewButton_7);
+		
+		
+	
+	}
+	
+	public void login(JFrame frame) {
+		JPanel panel = new JPanel();
 		panel.setBackground(new Color(0, 128, 192));
 		panel.setBounds(0, 0, 1134, 554);
 		frame.getContentPane().add(panel);
@@ -82,5 +128,34 @@ public class examen {
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 17));
 		btnNewButton.setBounds(698, 406, 190, 67);
 		panel.add(btnNewButton);
+		
+		JButton btnRegistrarse = new JButton("REGISTRARSE");
+		btnRegistrarse.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnRegistrarse.setBackground(Color.LIGHT_GRAY);
+		btnRegistrarse.setBounds(147, 406, 190, 67);
+		panel.add(btnRegistrarse);
+	}
+	
+	public void menu() {
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(128, 255, 255));
+		panel_1.setBounds(0, 0, 1134, 554);
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JButton btnNewButton_1 = new JButton("Alumnos");
+		btnNewButton_1.setBackground(new Color(192, 192, 192));
+		btnNewButton_1.setBounds(464, 70, 251, 109);
+		panel_1.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Docentes");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2.setBackground(new Color(192, 192, 192));
+		btnNewButton_2.setBounds(464, 263, 251, 109);
+		panel_1.add(btnNewButton_2);
+		
 	}
 }
